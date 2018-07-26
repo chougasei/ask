96b1ee27abfe2a69333978de991243452a5d09f6
gtech <gtech@macbook-test-04.local>
2018-07-26 15:54:34 +0800

    Initial Commit

---
+++ ask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ask
+//
+//  Created by gtech on 2018/7/26.
+//  Copyright © 2018年 gtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
